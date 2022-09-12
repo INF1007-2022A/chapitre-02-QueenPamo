233edfc46c698a80e5827a5c26b28b1d6db2974d
Pamela Bitar <pamelabitar4@hotmail.com>
2022-09-12 13:56:52 -0400

    version reliee a github

--- exercice.py
+++ exercice.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#on define une methode pour utiliser plus tard
 def majuscule(mot):
-    # TODO completer la fonction ici
-    return mot
-
+    nouveau_mot = ""
+    #on fait un loop qui va indiquer que pour chaque lettre dans le mot on fait la fonction
+    for lettre in mot:
+        #on fait une variable pour simplifier l'écriture, ca donne la valeur numérique ASCII de la lettre
+        temp = ord(lettre)
+        #indique que si la valeur numerique ascii est plus grande ou egale a 97 (les minuscules), on met en majuscule
+        if temp >= 97:
+            nouveau_mot += chr(temp-32)
+        #Sinon, on ajoute la lettre au nouveau mot sans la mettre en majuscule
+        else:
+            nouveau_mot += lettre
+    return nouveau_mot
 
 if __name__ == '__main__':
     mots = [
